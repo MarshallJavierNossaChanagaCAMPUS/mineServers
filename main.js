aaa3592ee8c0fae2e2bf0ef70231650904f25e0a
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-05-05 14:25:39 -0500

    feat: :sparkles: Add servers working!!!

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-let url = "https://mcapi.us/server/status?ip=mc.universocraft.com";
+
 let servers = document.querySelector(".servers");
 let server = [];
+let header = document.querySelector("#header");
+let modal = document.querySelector(".modal_padre");
+let anadirServidor = document.querySelector("#anadir_servidor");
+let guardarServidor = document.querySelector("#guardar_servidor");
+let cancelarServidor = document.querySelector("#cancelar_servidor");
 
-const listAPI = async function () {
+
+/* const listAPI = async function () {
     try {
         const data = await fetch(url);
         const data2 = await data.json();
@@ -13,9 +19,7 @@ const listAPI = async function () {
     }
 }
 
-console.log(server);
-
-const showServer = async function () {
+ const showServer = async function () {
     await listAPI();
     servers.insertAdjacentHTML("beforeend", `
     <div class="server">
@@ -23,7 +27,7 @@ const showServer = async function () {
         <div class="server_padre">
             <div class="server_info">
                 <h2>Servidor de minecraft</h2>
-                <h2>777/777</h2>
+                <h2>${server[0].players.now}/${server[0].players.max} <img class="senal-icon" src="../img/signal.png" width="35px"></h2>
             </div>
             <div class="server_text">
                 <h2>${server[0].motd}</h2>
@@ -31,7 +35,64 @@ const showServer = async function () {
         </div>
     </div>
 `)
-}
+} 
 
-showServer()
+showServer() */
 
+anadirServidor.addEventListener("click", (e)=>{
+    e.preventDefault();
+    header.classList.add("modal_padre");
+    modal.classList.remove("modal_padre");
+})
+
+cancelarServidor.addEventListener("click", (e)=>{
+    e.preventDefault();
+    header.classList.remove("modal_padre");
+    modal.classList.add("modal_padre");
+})
+
+guardarServidor.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let name = document.querySelector('input[name="name"]').value;
+    let ip = document.querySelector('input[name="ip"]').value;
+    let url = `https://mcapi.us/server/status?ip=${ip}`;
+
+    const listAPI = async function () {
+        try {
+            const data = await fetch(url);
+            const data2 = await data.json();
+            server.unshift(data2)
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+     const showServer = async function () {
+        await listAPI();
+        servers.insertAdjacentHTML("beforeend", `
+        <div class="server">
+            <img src="${server[0].favicon}" width="15%">
+            <div class="server_padre">
+                <div class="server_info">
+                    <h2>${name}</h2>
+                    <h2>${server[0].players.now}/${server[0].players.max} <img class="senal-icon" src="../img/signal.png" width="35px"></h2>
+                </div>
+                <div class="server_text">
+                    <h2>${server[0].motd}</h2>
+                </div>
+            </div>
+        </div>
+    `)
+    } 
+    
+    showServer()
+
+    console.log(name);
+    console.log(ip);
+
+    header.classList.remove("modal_padre");
+    modal.classList.add("modal_padre");
+
+    console.log(server);
+})
