9a5552f6f506fa7c26be5e1fa3ba078e60010d0d
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-05-11 10:46:41 -0500

    feat: :bug: Ya no se hace el bucle
    Se solucionó el bucle del post en json server

--- components/my-server.js
+++ components/my-server.js
@@ -1,21 +1,21 @@
 let pathName = new URL(import.meta.url).pathname;
 let name = pathName.split("/").pop().replace(".js", "");
 
-export class myServer extends HTMLElement{
-    static async components(){
-        return (await(await fetch(pathName.replace(".js", ".html"))).text());
-    }
-    constructor(){
-        super();
-        this.attachShadow({mode: "open"}); 
-    }
-    async getJson(){
-        this.urlJson = `http://localhost:3000/servers`
-            try {
-                let res = await fetch(this.urlJson)
-                let data = await res.json();
-                console.log(data);
-                /* data.forEach(dataServers => {
+export class myServer extends HTMLElement {
+  static async components() {
+    return await (await fetch(pathName.replace(".js", ".html"))).text();
+  }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  async getJson() {
+    this.urlJson = `http://localhost:3000/servers`;
+    try {
+      let res = await fetch(this.urlJson);
+      let data = await res.json();
+      console.log(data);
+      /* data.forEach(dataServers => {
                   servers.insertAdjacentHTML(
                     "beforeend",     `
                     <div class="cards" id="${ip}">
@@ -33,92 +33,94 @@ export class myServer extends HTMLElement{
                   `
                   )
                 }) */
-              } catch (error) {
-                console.log(error);
-              }
+    } catch (error) {
+      console.log(error);
     }
-    connectedCallback(){
-        Promise.resolve(myServer.components()).then(html=>{
-            this.shadowRoot.innerHTML = html
+  }
+  connectedCallback() {
+    Promise.resolve(myServer.components()).then((html) => {
+      this.shadowRoot.innerHTML = html;
 
-            /* Querys selectores xd */
+      /* Querys selectores xd */
 
-            this.myIndex = document.querySelector("#myIndex");
-            this.myServer = document.querySelector("#myServer");
+      this.myIndex = document.querySelector("#myIndex");
+      this.myServer = document.querySelector("#myServer");
 
-            this.anadirServidor = this.shadowRoot.querySelector("#anadir_servidor");
+      this.anadirServidor = this.shadowRoot.querySelector("#anadir_servidor");
 
-            this.guardarServidor = this.shadowRoot.querySelector("#guardar_servidor");
-            this.cancelarServidor = this.shadowRoot.querySelector("#cancelar_servidor");
+      this.guardarServidor = this.shadowRoot.querySelector("#guardar_servidor");
+      this.cancelarServidor =
+        this.shadowRoot.querySelector("#cancelar_servidor");
 
-            this.cerrarServer = this.shadowRoot.querySelector("#cerrar_servers");
+      this.cerrarServer = this.shadowRoot.querySelector("#cerrar_servers");
 
-            /* Escuchadores de eventos */
+      /* Escuchadores de eventos */
 
-            this.anadirServidor.addEventListener("click", (e)=>{
-                e.preventDefault();
-                this.modal = this.shadowRoot.querySelector("#modalsito");
-                this.modal.classList.remove("modal_padre");
-            });
+      this.anadirServidor.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.modal = this.shadowRoot.querySelector("#modalsito");
+        this.modal.classList.remove("modal_padre");
+      });
 
-            this.cerrarServer.addEventListener("click", (e)=>{
-                e.preventDefault();
-                this.myIndex.classList.remove("modal_padre");
-                this.myServer.classList.add("modal_padre");
-            });
+      this.cerrarServer.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.myIndex.classList.remove("modal_padre");
+        this.myServer.classList.add("modal_padre");
+      });
 
-            /* Escuchador guardar servidor */
-        
-            this.guardarServidor.addEventListener("click", (e) => {
-                e.preventDefault();
-                e.stopPropagation()
-                this.form = this.shadowRoot.querySelector("#form");
-                this.formData = Object.fromEntries(new FormData(this.form));
-                console.log(this.formData);
+      /* Escuchador guardar servidor */
 
-                /* Url mineAPI & json server */
+      this.guardarServidor.addEventListener(
+        "click",
+        this.handleEvent.bind(this)
+      );
+    });
+  }
+  handleEvent(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.form = this.shadowRoot.querySelector("#form");
+    this.formData = Object.fromEntries(new FormData(this.form));
+    console.log(this.formData);
+  
+    /* Url mineAPI & json server */
+    this.url = `https://mcapi.us/server/status?ip=${this.formData.ip}`;
+    this.urlJson = `http://localhost:4000/servers`;
+  
+    /* Llama a la mineAPI */
+    this.listAPI = async function () {
+        try {
+          const data = await fetch(this.url);
+          const data2 = await data.json();
+          console.log(data2);
+          
+          // Verificar si los datos ya existen en el servidor
+          const existingData = await fetch(this.urlJson);
+          const existingData2 = await existingData.json();
+          const matchingData = existingData2.find((item) => item.ip === data2.ip);
+          if (matchingData) {
+            console.log("Los datos ya existen en el servidor");
+            return { message: "Los datos ya existen en el servidor" };
+          }
+          // Si los datos no existen, agregarlos al servidor
+          const res = fetch(this.urlJson, {
+            method: "POST",
+            body: JSON.stringify(data2),
+            headers: { "Content-Type": "application/json" },
+          });
+          return { message: "Datos agregados correctamente" };
+        } catch (error) {
+          console.log(error);
+          return { error: "Ocurrió un error al agregar los datos" };
+        }
+      };
 
-                this.url = `https://mcapi.us/server/status?ip=${this.formData.ip}`;
-                this.urlJson = `http://localhost:3000/servers`
-
-                /* Llama a la mineAPI */
-
-                this.listAPI = async function () {
-                    try {
-                    const data = await fetch(this.url);
-                    const data2 = await data.json();
-                    try {
-
-                        /* Postear datos mineAPI al json server */
-
-                        const res = await fetch(this.urlJson, {
-                            method: "POST",
-                            body: JSON.stringify(data2),
-                            headers: {"Content-Type": "application/json"}
-                        });
-                    } catch (error) {
-                        console.log(error);
-                    }
-                    } catch (error) {
-                    console.log(error);
-                    }
-                };
-
-                this.listAPI();
-
-                this.modal.classList.add("modal_padre");
-
-                return false;
-            });
-
-            this.cancelarServidor.addEventListener("click", (e) => {
-                e.preventDefault();
-                this.modal.classList.add("modal_padre");
-            });
-
-        })
-    }
-
+      this.listAPI();
+  
+    this.modal.classList.add("modal_padre");
+  
+    return false;
+  }
 }
 
 customElements.define(name, myServer);
